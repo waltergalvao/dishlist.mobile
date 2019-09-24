33dd5c36ab4c2a36bff558e159539f5a706b4741
Walter Galvão <walter.galvao@biper.com.br>
2019-09-24 14:17:37 -0700

    Componentized the Drawer

---
+++ src/components/_dumb/VNavigationButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-btn
+    flat
+    dense
+    round
+    aria-label="Menu"
+  >
+    <q-icon name="menu" />
+  </q-btn>
+</template>
+
+<script>
+export default {
+  name: "VNavigationMenu"
+};
+</script>
