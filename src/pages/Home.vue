ee55527acf1aac0e9ee8267897157703bccbe521
Jyot Sarup <jyotsarup93@gmail.com>
2019-11-09 18:52:31 -0800

    Home screen styling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,96 +1,149 @@
 <template>
-    <q-page class="search-restaurant">
-        <div class="fixed-center text-center">
-            <q-img
-                class="search-restaurant__qr shadow-10"
-                style="border-radius: 10px;"
-                v-on:click="scanQRcode"
-                :src="imageSrc"
-            >
-                <div
-                    class="absolute-bottom text-subtitle1 text-center q-pa-xs search-restaurant__overlay"
+    <q-page class="search-restaurant text-center">
+        <div class=" search-restaurant__searchContainer">
+            <div class="search-restaurant__qr width85 ">
+                <img
+                class="search-restaurant__qr_img"            
+                :src="scanQRImage"
                 >
-                    Scan QR Code
-                </div>
-            </q-img>
+            </div>
+            
             <q-btn
                 color="primary"
-                class="q-ma-lg"
+                class=" width85 search-restaurant__searchBtn"
+                v-on:click="scanQRcode"           
+                >SCAN QR CODE</q-btn
+            >
+                
+            <q-btn
+                color="primary"
+                class="width85 search-restaurant__searchBtn search-restaurant__searchNameBtn"
                 outline
                 :to="{name: 'restaurant.search'}"
-                >or search by name</q-btn
-            >
+                >SEARCH BY NAME</q-btn
+            >            
+        </div>
+
+        <div class="search-restaurant__bottom">
+            <q-img class="fixed-bottom responsive" :src="homeScreenBottom"/>
+            <q-img class="fixed-bottom responsive search-restaurant__dishImg" :src="homeScreenDishImg"></q-img>
+            <q-img class="fixed-bottom responsive search-restaurant__cofeeImg" :src="homeScreenCafe"></q-img>
+            <q-img class="fixed-bottom responsive search-restaurant__strawberryImg" :src="homeScreensStrawberryImg"></q-img>
+            <q-img class="fixed-bottom responsive search-restaurant__strawberry2Img" :src="homeScreensStrawberries2Img"></q-img>
         </div>
     </q-page>
 </template>
 
 <script>
-import {mapActions, mapState} from 'vuex';
+    
+    import {mapActions, mapState} from 'vuex';
 
-export default {
-    name: 'Home',
-    data() {
-        return {
-            restaurantId: 1,
-            restaurantList: [],
-            imageSrc: 'statics/qr-code.png',
-        };
-    },
-    created() {
-        this.$emit('updateTitle', 'DishList');
-    },
-    methods: {
-        ...mapActions({
-            fetchMenu: 'fetchMenu',
-        }),
-        scanQRcode() {
-            let combinedVueInstance = this;
-
-            combinedVueInstance.$q.loading.show({
-                delay: 400,
-            });
-
-            cordova.plugins.barcodeScanner.scan(
-                result => {
-                    this.$q.loading.hide();
-                    if (result && result.cancelled !== 1 && result.text) {
-                        combinedVueInstance.$store.dispatch('fetchMenu', {
-                            restaurantId: result.text,
-                        });
-                        combinedVueInstance.$router.push(
-                            '/restaurant/' + result.text + '/menu',
-                        );
-                    }
-                },
-                error => {
-                    this.$q.loading.hide();
-                    this.$q.notify(error);
-                },
-            );
+    export default {
+        name: 'Home',
+        data() {
+            return {
+                restaurantId: 1,
+                restaurantList: [],
+                scanQRImage: 'statics/qr-code.png',
+                homeScreenBottom: 'statics/home-screen-bottom.png',
+                homeScreenDishImg:'statics/home-screen-dish-img.png',
+                homeScreenCafe: 'statics/home-cafe.png',
+                homeScreensStrawberryImg: 'statics/home-strawberry-img.png',
+                homeScreensStrawberries2Img: 'statics/home-strawberries-2-img.png',
+            };
         },
-    },
-    computed: {
-        ...mapState({
-            featuredItems: state => state.menu.featuredItems,
-            place: state => state.menu.place,
-            categories: state => state.menu.categories,
-        }),
-    },
-    plugins: ['Loading'],
-};
+        created() {
+            this.$emit('updateTitle', 'DishList');
+        },
+        methods: {
+            ...mapActions({
+                fetchMenu: 'fetchMenu',
+            }),
+            scanQRcode() {
+                let vueMethodsObj = this;
+                cordova.plugins.barcodeScanner.scan(function(result) {
+                    console.log(result);
+                    vueMethodsObj.$store.dispatch('fetchMenu', {
+                        restaurantId: result.text,
+                    });
+                    vueMethodsObj.$router.push(
+                        '/restaurant/' + result.text + '/menu',
+                    );
+                });
+            },
+        },
+        computed: {
+            ...mapState({
+                featuredItems: state => state.menu.featuredItems,
+                place: state => state.menu.place,
+                categories: state => state.menu.categories,
+            }),
+        },
+    };
 </script>
 
 <style lang="scss">
-.search-restaurant {
-    &__overlay {
-        background: rgba(0, 0, 0, 0.8) !important;
-        font-weight: bold;
-        font-size: 1.3em;
-        text-transform: uppercase;
+    .search-restaurant {
+
+        &__overlay {
+            font-weight: bold;
+            font-size: 1.3em;
+            text-transform: uppercase;
+        }
+
+        &__qr {        
+            background:#cdcdcd;       
+            height: 12rem;
+            border-radius: 3px;
+            margin-top: 2rem;
+        }
+
+        &__searchBtn {
+            height: 3.5rem;
+        }
+
+        &__searchNameBtn {
+            margin-top: 1rem;
+        }
+        &__qr_img {
+            height: 80%;
+            width: auto;
+            position: relative;;
+            top:50%;
+            transform: translateY(-50%);
+        }   
+
+        &__dishImg {
+            width:50%;
+            margin:auto;
+        }
+        &__cofeeImg {
+            width: 10%;
+            position:absolute;
+            bottom: 3.5rem;
+            left: 1rem;
+        }
+
+        &__strawberryImg {
+            width: 15%;
+            position:absolute;
+            bottom: 6rem;
+            left: 80%;
+        }
+
+        &__strawberry2Img {
+            position: absolute;
+            bottom: 8rem;
+            width: 50%;
+            left: 30%;
+        }
     }
 
-    &__qr {
-        margin: 1em 0;
+    div.search-restaurant__searchContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
     }
-}
+
 </style>
