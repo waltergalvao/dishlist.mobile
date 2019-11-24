52399260e33bdb4736d67fb0784095cf138ddba5
Walter Galvão <walterbgneto@gmail.com>
2019-11-23 16:04:27 -0800

    Prettier fixes

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -4,47 +4,47 @@
             <div class="child1"></div>
             <div class="child2">
                 <h5><b>Contact Us</b></h5>
-                <p>Call or email us to get in touch </p>
-                 <q-list dense padding>
-                <q-item class="contact-detail">
-                    <img
-                        class="contact-icon"
-                        src="~assets/contact-email.png"
-                        />
-                    <q-item-section>
-                    info@dishlist.ca
-                    </q-item-section>
-                </q-item>
-
-                <q-item class="contact-detail">
-                    <img
-                        class="contact-icon"
-                        src="~assets/contact-call.png"
-                        />
-                    <q-item-section>
-                        604.323.5511
-                    </q-item-section>
-                </q-item>
-                <q-item class="contact-detail">
-                    <img
-                        class="contact-icon"
-                        src="~assets/contact-web.png"
-                        />
-                    <q-item-section>
-                        http://dishlist.wmdd.ca/
-                    </q-item-section>
-                </q-item>
-                  <q-item class="contact-detail">
+                <p>Call or email us to get in touch</p>
+                <q-list dense padding>
+                    <q-item class="contact-detail">
                         <img
-                        class="contact-icon"
-                        src="~assets/contact-location.png"
+                            class="contact-icon"
+                            src="~assets/contact-email.png"
                         />
                         <q-item-section>
-                        100 West 49th Avenue, Vancouver, BC
-                        <br>Canada V5Y 2Z6
+                            info@dishlist.ca
                         </q-item-section>
                     </q-item>
-            </q-list>
+
+                    <q-item class="contact-detail">
+                        <img
+                            class="contact-icon"
+                            src="~assets/contact-call.png"
+                        />
+                        <q-item-section>
+                            604.323.5511
+                        </q-item-section>
+                    </q-item>
+                    <q-item class="contact-detail">
+                        <img
+                            class="contact-icon"
+                            src="~assets/contact-web.png"
+                        />
+                        <q-item-section>
+                            http://dishlist.wmdd.ca/
+                        </q-item-section>
+                    </q-item>
+                    <q-item class="contact-detail">
+                        <img
+                            class="contact-icon"
+                            src="~assets/contact-location.png"
+                        />
+                        <q-item-section>
+                            100 West 49th Avenue, Vancouver, BC
+                            <br />Canada V5Y 2Z6
+                        </q-item-section>
+                    </q-item>
+                </q-list>
             </div>
         </div>
     </q-page>
@@ -64,44 +64,43 @@ export default {
 </script>
 
 <style lang="scss">
-    .parent{
-        position: relative;
-        top:50px;
-        height:80vh;
-        width:100%;
+.parent {
+    position: relative;
+    top: 50px;
+    height: 80vh;
+    width: 100%;
+}
+.child1 {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    height: 80%;
+    border: 10px solid $primary;
+}
+.child2 {
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    height: 60%;
+    background-color: white;
+    padding-top: 10px;
+    padding-left: 10px;
+    > h5 {
+        margin: 0;
+        padding: 10px 0 20px 0;
     }
-    .child1{
-        position:absolute;
-        top:50%;
-        left:50%;
-        transform: translate(-50%,-50%);
-        width:80%;
-        height:80%;
-        border: 10px solid $primary;
-    }
-    .child2{
-        position:absolute;
-        top:50%;
-        left:45%;
-        transform: translate(-50%,-50%);
-        width:80%;
-        height:60%;
-        background-color:white;
-        padding-top:10px;
-        padding-left:10px;
-        >h5{
-            margin:0;
-            padding:10px 0 20px 0;
-        }
-    }
-    .contact-icon{
-        width:30px;
-        height:30px;
-        margin-right:10px;
-    }
-    .contact-detail{
-        margin-bottom:20px;
-        padding-left:0px !important;
-    }
-
+}
+.contact-icon {
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+}
+.contact-detail {
+    margin-bottom: 20px;
+    padding-left: 0px !important;
+}
 </style>
