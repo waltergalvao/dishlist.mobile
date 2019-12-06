3d306d92c994028b3626e4d90918708d3f962e1b
Walter Galvão <walterbgneto@gmail.com>
2019-12-06 10:20:20 -0800

    Added links to Credits

--- src/pages/Credits.vue
+++ src/pages/Credits.vue
@@ -4,28 +4,182 @@
             <q-card>
                 <q-card-section>
                     <p>Below is a list of images and Icons used</p>
-                    <p>Photo by Hisu lee on Unsplash https://unsplash.com/photos/bQLCyj-9-tk</p>
-                    <p>Photo by Toa Heftiba on Unsplash https://unsplash.com/photos/HPX4lhOoIDA</p>
-                    <p>Photo by Debby Hudson on Unsplash https://unsplash.com/photos/bNcMqYvvIJI</p>
-                    <p>Icon made by Alfredo Hernandez from www.flaticon.com  https://www.flaticon.com/free- icon/email_458866?term=mail&amp;page=5&amp;position=25</p>
-                    <p>Icon made by Alfredo Hernandez from www.flaticon.com https://www.flaticon.com/free-icon/call_399403</p>
-                    <p>Icon made by Alfredo Hernandez from www.flaticon.com https://www.flaticon.com/free-icon/www_403991?term=www&page=1&position=41</p>
-                    <p>Icon made by Alfredo Hernandez from www.flaticon.com https://www.flaticon.com/free-icon/email_458866?term=mail&page=5&position=25 </p>
-                    <p>Icon made by Freepik from www.flaticon.com https://www.flaticon.com/free-icon/arrow-point-to-right_32213?term=arrow&page=1&position=2 </p>
-                    <p>Icons by Quasar Material UI Icons https://quasar.dev/vue-components/icon</p>
-                    <p> QR code icon on Home by FreePik https://www.flaticon.com/free-icon/qr-code_1058992?term=qr&page=1&position=8 </p>
-                    <p>Photo by Miguel Maldonado on Unsplash - https://unsplash.com/photos/qom5MPOER-I</p>
-                    <p>Photo by j on Unsplash - https://unsplash.com/photos/lbxLlgi_8_w</p>
-                    <p>Photo by Matthew Hamilton on Unsplash - https://unsplash.com/photos/RA4mwm9_jKA</p>
-                    <p>Photo by Clem Onojeghuo on Unsplash - https://unsplash.com/photos/zlABb6Gke24</p>
-                    <p>Photo by Pratiksha Mohanty on Unsplash - https://unsplash.com/photos/e0Pr8LmvHTM</p>
-                    <p>Photo by Toa Heftiba on Unsplash - https://unsplash.com/photos/WPRuXYLNY68</p>
-                    <p>Photo by Alison Marras on Unsplash - https://unsplash.com/photos/O0H6MVtAP9M</p>
-                    <p>Photo by Jenny Pace on Unsplash - https://unsplash.com/photos/K5IUb0kBZZ8</p>
-                    <p>Photo by Anna Pelzer on Unsplash - https://unsplash.com/photos/IGfIGP5ONV0</p>
-                    <p>Photo by Hermes Rivera on Unsplash - https://unsplash.com/photos/OzBLe_Eg1mg</p>
-                    <p>Photo by Nadine Primeau on Unsplash - https://unsplash.com/photos/-ftWfohtjNw</p>
-                    <p>Photo by Jasmin Schreiber on Unsplash - https://unsplash.com/photos/V2Kw-YC7Cls</p>
+                    <p>
+                        Photo by Hisu lee on Unsplash
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/bQLCyj-9-tk"
+                            >https://unsplash.com/photos/bQLCyj-9-tk</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Toa Heftiba on Unsplash
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/HPX4lhOoIDA"
+                            >https://unsplash.com/photos/HPX4lhOoIDA</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Debby Hudson on Unsplash
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/bNcMqYvvIJI"
+                            >https://unsplash.com/photos/bNcMqYvvIJI</a
+                        >
+                    </p>
+                    <p>
+                        Icon made by Alfredo Hernandez from www.flaticon.com
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/email_458866?term=mail&amp;page=5&amp;position=25"
+                            >https://www.flaticon.com/free-icon/email_458866?term=mail&amp;page=5&amp;position=25</a
+                        >
+                    </p>
+                    <p>
+                        Icon made by Alfredo Hernandez from www.flaticon.com
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/call_399403"
+                            >https://www.flaticon.com/free-icon/call_399403</a
+                        >
+                    </p>
+                    <p>
+                        Icon made by Alfredo Hernandez from www.flaticon.com
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/www_403991?term=www&page=1&position=41"
+                            >https://www.flaticon.com/free-icon/www_403991?term=www&page=1&position=41</a
+                        >
+                    </p>
+                    <p>
+                        Icon made by Alfredo Hernandez from www.flaticon.com
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/email_458866?term=mail&page=5&position=25 "
+                            >https://www.flaticon.com/free-icon/email_458866?term=mail&page=5&position=25
+                        </a>
+                    </p>
+                    <p>
+                        Icon made by Freepik from www.flaticon.com
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/arrow-point-to-right_32213?term=arrow&page=1&position=2 "
+                            >https://www.flaticon.com/free-icon/arrow-point-to-right_32213?term=arrow&page=1&position=2
+                        </a>
+                    </p>
+                    <p>
+                        Icons by Quasar Material UI Icons
+                        <a
+                            target="_blank"
+                            href="https://quasar.dev/vue-components/icon"
+                            >https://quasar.dev/vue-components/icon</a
+                        >
+                    </p>
+                    <p>
+                        QR code icon on Home by FreePik
+                        <a
+                            target="_blank"
+                            href="https://www.flaticon.com/free-icon/qr-code_1058992?term=qr&page=1&position=8 "
+                            >https://www.flaticon.com/free-icon/qr-code_1058992?term=qr&page=1&position=8
+                        </a>
+                    </p>
+                    <p>
+                        Photo by Miguel Maldonado on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/qom5MPOER-I"
+                            >https://unsplash.com/photos/qom5MPOER-I</a
+                        >
+                    </p>
+                    <p>
+                        Photo by j on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/lbxLlgi_8_w"
+                            >https://unsplash.com/photos/lbxLlgi_8_w</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Matthew Hamilton on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/RA4mwm9_jKA"
+                            >https://unsplash.com/photos/RA4mwm9_jKA</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Clem Onojeghuo on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/zlABb6Gke24"
+                            >https://unsplash.com/photos/zlABb6Gke24</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Pratiksha Mohanty on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/e0Pr8LmvHTM"
+                            >https://unsplash.com/photos/e0Pr8LmvHTM</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Toa Heftiba on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/WPRuXYLNY68"
+                            >https://unsplash.com/photos/WPRuXYLNY68</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Alison Marras on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/O0H6MVtAP9M"
+                            >https://unsplash.com/photos/O0H6MVtAP9M</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Jenny Pace on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/K5IUb0kBZZ8"
+                            >https://unsplash.com/photos/K5IUb0kBZZ8</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Anna Pelzer on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/IGfIGP5ONV0"
+                            >https://unsplash.com/photos/IGfIGP5ONV0</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Hermes Rivera on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/OzBLe_Eg1mg"
+                            >https://unsplash.com/photos/OzBLe_Eg1mg</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Nadine Primeau on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/-ftWfohtjNw"
+                            >https://unsplash.com/photos/-ftWfohtjNw</a
+                        >
+                    </p>
+                    <p>
+                        Photo by Jasmin Schreiber on Unsplash -
+                        <a
+                            target="_blank"
+                            href="https://unsplash.com/photos/V2Kw-YC7Cls"
+                            >https://unsplash.com/photos/V2Kw-YC7Cls</a
+                        >
+                    </p>
                 </q-card-section>
             </q-card>
         </div>
@@ -33,10 +187,10 @@
 </template>
 
 <script>
-    export default {
-        name: 'Credits',
-        created() {
-            this.$emit('updateTitle', 'Credits');
-        },
-    };
+export default {
+    name: 'Credits',
+    created() {
+        this.$emit('updateTitle', 'Credits');
+    },
+};
 </script>
